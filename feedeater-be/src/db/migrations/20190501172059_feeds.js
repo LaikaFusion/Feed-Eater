04946b147ff8b95ce3c5ee6e7e14418587109a76
LaikaFusion <newrad4@gmail.com>
2019-05-01 17:44:19 -0400

    added feeds table

--- feedeater-be/src/db/migrations/20190501172059_feeds.js
+++ feedeater-be/src/db/migrations/20190501172059_feeds.js
@@ -1,8 +1,11 @@
-
-exports.up = function(knex, Promise) {
-  
+exports.up = function(knex) {
+  return knex.schema.createTable("feeds", function(feed) {
+    feed.incriments("feedId");
+    feed.string("url");
+    feed.timestamp("lastChecked");
+  });
 };
 
-exports.down = function(knex, Promise) {
-  
+exports.down = function(knex) {
+  return knex.schema.dropTable("feeds");
 };
