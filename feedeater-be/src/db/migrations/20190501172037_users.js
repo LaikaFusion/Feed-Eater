9c43576ef9f49316190543514d509c7e3a52bd73
LaikaFusion <newrad4@gmail.com>
2019-05-01 17:34:59 -0400

    created migration for users

---
+++ feedeater-be/src/db/migrations/20190501172037_users.js
@@ -0,0 +1,11 @@
+exports.up = function(knex) {
+  return knex.schema.createTable("users", function(user) {
+    user.incriments("userId");
+    user.string("username");
+    user.string("hash");
+  });
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTable("users");
+};
