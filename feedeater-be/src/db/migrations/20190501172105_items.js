702e82d022f83ef3c316acb0e5f40b961fe60c4c
LaikaFusion <newrad4@gmail.com>
2019-05-01 17:47:01 -0400

    items migration added

--- feedeater-be/src/db/migrations/20190501172105_items.js
+++ feedeater-be/src/db/migrations/20190501172105_items.js
@@ -1,8 +1,11 @@
-
-exports.up = function(knex, Promise) {
-  
+exports.up = function(knex) {
+  return knex.schema.createTable("items", function(item) {
+    item.incriments("itemId");
+    item.json("content");
+    item.timestamp("created_at");
+  });
 };
 
-exports.down = function(knex, Promise) {
-  
+exports.down = function(knex) {
+  return knex.schema.dropTable("items");
 };
