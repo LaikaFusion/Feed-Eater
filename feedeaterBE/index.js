6a3af14f6ba939ff678272d691e556d74eed38c9
LaikaFusion <newrad4@gmail.com>
2019-05-28 17:11:15 -0400

    auth is a mess stuck there for now

--- feedeaterBE/index.js
+++ feedeaterBE/index.js
@@ -1,23 +1,60 @@
-const express = require('express');
-const { ApolloServer, gql } = require('apollo-server-express');
+const express = require("express");
+const passport = require("passport");
+var Strategy = require("passport-local").Strategy;
+const bodyParser = require("body-parser");
+const { ApolloServer, gql } = require("apollo-server-express");
+
+const HEADER_NAME = "authorization";
 
 // Construct a schema, using GraphQL schema language
 const typeDefs = gql`
   type Query {
-    hello: String
+    me: User
+  }
+
+  type User {
+    id: ID!
+    username: String!
   }
 `;
 
 // Provide resolver functions for your schema fields
 const resolvers = {
   Query: {
-    hello: () => 'Hello world!',
+    me: (root, args, context) => context.currentUser
   },
+  User: {
+    id: user => user._id,
+    username: user => user.username
+  }
 };
 
-const server = new ApolloServer({ typeDefs, resolvers });
-
 const app = express();
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: async ({ req }) => {
+    let authToken = null;
+    let currentUser = null;
+
+    try {
+      authToken = req.headers[HEADER_NAME];
+
+      if (authToken) {
+        currentUser = authToken;
+        console.log(authToken);
+      }
+    } catch (e) {
+      console.warn(`Unable to authenticate using auth token: ${authToken}`);
+    }
+
+    return {
+      authToken,
+      currentUser
+    };
+  }
+});
 server.applyMiddleware({ app });
 
 app.listen({ port: 4000 }, () =>
