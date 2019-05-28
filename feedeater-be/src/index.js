c026adbc5aae52736c5dcb54e52ac206f0b0c71c
LaikaFusion <newrad4@gmail.com>
2019-05-28 14:44:14 -0400

    last express commit

--- feedeater-be/src/index.js
+++ feedeater-be/src/index.js
@@ -5,7 +5,6 @@ const app = express();
 const port = process.env.PORT;
 
 app.use(cors);
-
 app.listen(port, function() {
   // eslint-disable-next-line no-console
   console.log(`CORS-enabled web server listening on port ${port}`);
